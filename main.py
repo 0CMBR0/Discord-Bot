cfe120659f7f37151ca27636bccb9519a9a8a255
Areeb Khan <110183060+0CMBR0@users.noreply.github.com>
2022-09-09 19:46:21 -0400

    Update main.py

--- main.py
+++ main.py
@@ -18,33 +18,3 @@ if __name__ == '__main__':
     client.load_extension(extension)
 
 client.run('MTAwMzM2MzgyNDQ5OTQzNzc4MA.GApgIF.f1YkiPaZ2jqT3N4LwVD3so2XqSuAufJLyjeF2A')
-
-
-"""
-@client.command()
-async def load (ctx, extension):
-  client.load_extension(f'cogs.{extension}')
-
-@client.command()
-async def reload (ctx, extension):
-  client.unload_extension(f'cogs.{extension}')
-  client.load_extension(f'cogs.{extension}')
-
-@client.command()
-async def unload (ctx, extension):
-  client.unload_extension(f'cogs.{extension}')
-
-for filename in os.listdir('.\cogs'):
-  if filename.endswith('.py'):
-    client.load_extension(f'cogs.{filename[:-3]}')
-
-"""
-"""
-@client.event
-async def on_member_join(member):
-  print (f'{member} has joined a sever.')
-
-@client.event
-async def on_member_remove(member):
-  print (f'{member} has boi yoing yoing off the server fam.')
-"""
